d45e4adf5ec1690f5bf743a62f558213a19f2eb1
student <student@DESKTOP-4ARRLB2>
2022-09-17 15:22:14 +0800

    upload 03:22

---
+++ mvnspringjdbcquery_0917/src/main/java/demo/spring/model/CoffeeDAO.java
@@ -0,0 +1,35 @@
+package demo.spring.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+
+public class CoffeeDAO {
+	private JdbcTemplate template;
+
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+
+	public List<Coffee> getAllEmployeesRowMapper() {
+		RowMapper<Coffee> rows = new RowMapper<Coffee>() {
+			@Override
+			public Coffee mapRow(ResultSet rs, int rownumber) throws SQLException {
+				Coffee c = new Coffee();
+				//upper or lower doesn't matter
+				c.setCofName(rs.getString("cof_name"));
+				c.setSupId(rs.getInt("sup_id"));
+				c.setPrice(rs.getDouble("price"));
+				c.setSales(rs.getInt("sales"));
+				c.setTotal(rs.getInt("total"));
+				return c;
+			}
+		};
+		return template.query("select * from classicmodels.Coffees", rows);
+	}
+}
